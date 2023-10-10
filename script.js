6df6a0d486b308bfefffbd8fb48a8a42b2bef3eb
Harmornic <chekge.88899@gmail.com>
2023-10-11 02:50:32 +0700

    Final

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function toggleMenu() {
+function toggleMenu(isSubMenu=false) {
   const menu = document.querySelector('.menu');
+  if(isSubMenu && !menu.classList.contains("show")) return
+  
   menu.classList.toggle("show");
 }
